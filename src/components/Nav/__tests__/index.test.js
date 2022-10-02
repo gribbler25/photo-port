a18c60000ce96bd8dc9e8bde735e40892a4f9607
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-10-02 16:32:41 -0500

    finished with About/Nav tests

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, cleanup, getAllByLabelText } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Nav from "..";
+
+describe("Nav component", () => {
+  // baseline test
+  it("renders", () => {
+    render(<Nav />);
+  });
+});
+
+it("matches snapshot", () => {
+  const { asFragment } = render(<Nav />);
+  expect(asFragment).toMatchSnapshot();
+});
+
+describe("emoji is visible", () => {
+  it("inserts emoji into the h2", () => {
+    const { getByLabelText } = render(<Nav />);
+    expect(getByLabelText("camera")).toHaveTextContent("📸");
+  });
+});
+
+describe("links are visible", () => {
+  it("inserts text into links", () => {
+    const { getByTestId } = render(<Nav />);
+    expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
+    expect(getByTestId("about")).toHaveTextContent("About me");
+  });
+});
+
+afterEach(cleanup);
