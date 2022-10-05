cc7ea1b8e94ed7eecf3115c3c9fedb6171a15c52
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-10-05 14:47:03 -0500

    minor modifications

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,7 +4,10 @@ import coverImage from "../../assets/cover/cover-image.jpg";
 const About = () => {
   return (
     <section className="my-5">
-      <h1 id="about">Who am I ??</h1>
+      <h1 id="about">
+        Who am I ? I'm a world-renowned photographer from Poughkeepsie, New
+        York.
+      </h1>
       <img
         src={coverImage}
         className="my-2"
