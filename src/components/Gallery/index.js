5170b4841edc4e58be84970022de7e55163a6b89
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-10-05 12:08:04 -0500

    minor modifications

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers.js";
 import PhotoList from "../PhotoList";
 
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 const Modal = ({ onClose, currentPhoto }) => {
   const { name, category, description, index } = currentPhoto;
   return (
