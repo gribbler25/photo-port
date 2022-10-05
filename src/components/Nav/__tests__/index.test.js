3b3d568b43d5c715713cf8656f78a6806a2b1b0b
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-10-04 21:41:34 -0500

    all tests pass for rendering all components with conditionals functioning for a SPA

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -8,6 +8,8 @@ const categories = [
 ];
 const mockCurrentCategory = jest.fn();
 const mockSetCurrentCategory = jest.fn();
+const mockContactSelected = jest.fn();
+const mockSetContactSelected = jest.fn();
 
 describe("Nav component", () => {
   // baseline test
@@ -17,6 +19,8 @@ describe("Nav component", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
   });
@@ -28,6 +32,8 @@ it("matches snapshot", () => {
       categories={categories}
       setCurrentCategory={mockSetCurrentCategory}
       currentCategory={mockCurrentCategory}
+      contactSelected={mockContactSelected}
+      setContactSelected={mockSetContactSelected}
     />
   );
   expect(asFragment).toMatchSnapshot();
@@ -40,6 +46,8 @@ describe("emoji is visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
     expect(getByLabelText("camera")).toHaveTextContent("📸");
@@ -53,6 +61,8 @@ describe("links are visible", () => {
         categories={categories}
         setCurrentCategory={mockSetCurrentCategory}
         currentCategory={mockCurrentCategory}
+        contactSelected={mockContactSelected}
+        setContactSelected={mockSetContactSelected}
       />
     );
     expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
